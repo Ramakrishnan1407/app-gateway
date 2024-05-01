a6419fef34c150c4f3c978d2959e5f52606db19d
rrajaram2 <rrajaram2@delhaize.be>
2024-05-01 18:25:41 +0200

    app-gateway

--- main.tf
+++ main.tf
@@ -146,11 +146,11 @@ resource "azurerm_application_gateway" "network" {
     port = 80
   }
   
-  
+  /*
   frontend_ip_configuration {
     name                 = local.frontend_ip_configuration_name
     public_ip_address_id = azurerm_public_ip.example.id
-  }
+  }*/
 
   frontend_ip_configuration {
     name                          = "fip-private"
