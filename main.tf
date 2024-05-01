181924c3fbd3ba6f2ef6274340389f9c651c68cd
rrajaram2 <rrajaram2@delhaize.be>
2024-05-01 17:47:35 +0200

    app-gateway

--- main.tf
+++ main.tf
@@ -127,10 +127,12 @@ resource "azurerm_application_gateway" "network" {
   name                = "example-appgateway2"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
+  
+  firewall_policy_id   = azurerm_web_application_firewall_policy.example.id
 
   sku {
-    name     = "Standard_v2"
-    tier     = "Standard_v2"
+    name     = "WAF_v2"
+    tier     = "WAF_v2"
     capacity = 2
   }
 
@@ -144,11 +146,11 @@ resource "azurerm_application_gateway" "network" {
     port = 80
   }
   
-  /*
+  
   frontend_ip_configuration {
     name                 = local.frontend_ip_configuration_name
     public_ip_address_id = azurerm_public_ip.example.id
-  }*/
+  }
 
   frontend_ip_configuration {
     name                          = "fip-private"
@@ -187,15 +189,17 @@ resource "azurerm_application_gateway" "network" {
   }
 }  
 
-
+/*
 resource "azurerm_application_gateway" "network2" {
   name                = "example-appgateway3"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
 
+  firewall_policy_id   = azurerm_web_application_firewall_policy.example.id
+
   sku {
-    name     = "Standard_v2"
-    tier     = "Standard_v2"
+    name     = "WAF_v2"
+    tier     = "WAF_v2"
     capacity = 2
   }
 
@@ -243,4 +247,97 @@ resource "azurerm_application_gateway" "network2" {
     backend_http_settings_name = local.http_setting_name
   }
 }  
+*/
 
+resource "azurerm_web_application_firewall_policy" "example" {
+  name                = "example-wafpolicy"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  custom_rules {
+    name      = "Rule1"
+    priority  = 1
+    rule_type = "MatchRule"
+
+    match_conditions {
+      match_variables {
+        variable_name = "RemoteAddr"
+      }
+
+      operator           = "IPMatch"
+      negation_condition = false
+      match_values       = ["192.168.1.0/24", "10.0.0.0/24"]
+    }
+
+    action = "Block"
+  }
+
+  custom_rules {
+    name      = "Rule2"
+    priority  = 2
+    rule_type = "MatchRule"
+
+    match_conditions {
+      match_variables {
+        variable_name = "RemoteAddr"
+      }
+
+      operator           = "IPMatch"
+      negation_condition = false
+      match_values       = ["192.168.1.0/24"]
+    }
+
+    match_conditions {
+      match_variables {
+        variable_name = "RequestHeaders"
+        selector      = "UserAgent"
+      }
+
+      operator           = "Contains"
+      negation_condition = false
+      match_values       = ["Windows"]
+    }
+
+    action = "Block"
+  }
+
+  policy_settings {
+    enabled                     = true
+    mode                        = "Prevention"
+    request_body_check          = true
+    file_upload_limit_in_mb     = 100
+    max_request_body_size_in_kb = 128
+  }
+
+  managed_rules {
+    exclusion {
+      match_variable          = "RequestHeaderNames"
+      selector                = "x-company-secret-header"
+      selector_match_operator = "Equals"
+    }
+    exclusion {
+      match_variable          = "RequestCookieNames"
+      selector                = "too-tasty"
+      selector_match_operator = "EndsWith"
+    }
+
+    managed_rule_set {
+      type    = "OWASP"
+      version = "3.2"
+      rule_group_override {
+        rule_group_name = "REQUEST-920-PROTOCOL-ENFORCEMENT"
+        rule {
+          id      = "920300"
+          enabled = true
+          action  = "Log"
+        }
+
+        rule {
+          id      = "920440"
+          enabled = true
+          action  = "Block"
+        }
+      }
+    }
+  }
+}
